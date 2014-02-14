63b3dd206a0d01108cdb70e48c8d11cfd10f7581
Carlos Vinicius <abel.road@gmail.com>
2014-02-14 14:58:46 -0200

    ajax json

--- project/assets/www/js/main.js
+++ project/assets/www/js/main.js
@@ -1,26 +1,42 @@
-$(document).on("pageinit", "#main", function(){
-  $("a").on("click", function(){
-    var clicado = $(this).attr('rel');
-    getClicado(clicado);
-  })
-  function getClicado(clicked){
-    if(clicked=="AC"){
-      $.mobile.navigate('#feed');
-      $(".headBar h1").html("AnimeCodex Feed");
-      $.getJSON("https://ajax.googleapis.com/ajax/services/feed/find?v=1.0&q=codecademy&callback=?", xmlParser(response));
-      //$("#segundoContent").html("AnimeCodex.Com");
-    }
-    else if(clicked=="ACN"){
-      $.mobile.navigate('#feed');
-      $(".headBar h1").html("AnimeCodexNetwork Feed")
-      //$("#segundoContent").html("AnimeCodexNetwork.Com");
-    }
-    
-  }
+$(document).on("pageinit", "#main", function() {
+    $("a").on("click", function() {
+        var clicado = $(this).attr('rel');
+        getClicado(clicado);
+    })
 
-  
-  function xmlParser(xml) {
-   $('#segundoContent').html(JSON.stringify(response));
-  }
+    function getClicado(clicked) {
+        if (clicked == "AC") {
+            $.mobile.navigate('#feed');
+            $(".headBar h1").html("AnimeCodex Feed");
+
+            var url = "https://ajax.googleapis.com/ajax/services/feed/find?v=1.0&q=codecademy&callback=?"
+            $.ajax({
+                url: url,
+                type: 'GET',
+                dataType: 'json',
+            })
+                .done(function() {
+                    console.log("success");
+                })
+                .fail(function() {
+                    console.log("error");
+                })
+                .always(function() {
+                    console.log("complete");
+                });
+
+
+            //$("#segundoContent").html("AnimeCodex.Com");
+        } else if (clicked == "ACN") {
+            $.mobile.navigate('#feed');
+            $(".headBar h1").html("AnimeCodexNetwork Feed")
+            //$("#segundoContent").html("AnimeCodexNetwork.Com");
+        }
+
+    }
+
+
+    function xmlParser(xml) {
+        $('#segundoContent').html(JSON.stringify(response));
+    }
 })
-
