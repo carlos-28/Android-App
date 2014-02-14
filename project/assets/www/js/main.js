f230de17f66d156ff4bb6e6779d0e03e6383835b
carlos-28 <abel.road@gmail.com>
2014-02-13 22:40:37 -0200

    mais mudancas

--- project/assets/www/js/main.js
+++
@@ -1,4 +0,0 @@
- document.addEventListener("deviceready", onDeviceReady, false);
-            function onDeviceReady() {
-                $("#content").html("Hello, World! Demo  JQuery Mobile com PhoneGap 2!");
-            }
