b7793901970353120be1f90e577c7fa8f7d7d945
Carlos Vinicius <abel.road@gmail.com>
2014-02-14 14:48:02 -0200

    json update

--- project/assets/www/js/main.js
+++ project/assets/www/js/main.js
@@ -7,39 +7,20 @@ $(document).on("pageinit", "#main", function(){
     if(clicked=="AC"){
       $.mobile.navigate('#feed');
       $(".headBar h1").html("AnimeCodex Feed");
+      $.getJSON("https://ajax.googleapis.com/ajax/services/feed/find?v=1.0&q=codecademy&callback=?", xmlParser(response));
       //$("#segundoContent").html("AnimeCodex.Com");
     }
     else if(clicked=="ACN"){
       $.mobile.navigate('#feed');
       $(".headBar h1").html("AnimeCodexNetwork Feed")
-      $("#segundoContent").html("AnimeCodexNetwork.Com");
+      //$("#segundoContent").html("AnimeCodexNetwork.Com");
     }
     
   }
 
-  $.getJSON("https://ajax.googleapis.com/ajax/services/feed/find?v=1.0&q=codecademy&callback=?", xmlParser(response));
+  
   function xmlParser(xml) {
    $('#segundoContent').html(JSON.stringify(response));
-  }
-  
-
-  function hideMain() {
-    $("#mainHdr").hide();
-    $("#mainContent").hide();
-    $("#mainFtr").hide();
-  }
-  function hideFeed() {
-    $("#feedHdr").hide();
-    $("#segundoContent").hide();
-    $("#feedFtr").hide();
-  }
-
-  function showProgress() {
-    hideMain();
-    hideFeed();
-    $("#progressHdr").show();
-    $("#progressContent").show();
-    $("#ftrProgress").show();
   }
 })
 
