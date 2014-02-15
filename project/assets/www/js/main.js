f9ac56e69c262b42fdbb39b4c2b277fef9d71625
carlos-28 <abel.road@gmail.com>
2014-02-15 02:26:36 -0200

    feeds grid

--- project/assets/www/js/main.js
+++ project/assets/www/js/main.js
@@ -2,25 +2,30 @@ $(document).on("pageinit", "#main", function() {
     var codex = "http://animecodex.com.br/api/get_recent_posts/";
     var network = "http://animecodexnetwork.com.br";
     var feedContent = $("#feedContent");
+    var feedHome = $("#feedHome");
+    var main = $("#main");
+    var item = "";
 
 
 
 
 
 
-    $("a").on("click", function() {
+    $("#main a").on("click", function() {
         var clicado = $(this).attr('rel');
         if (clicado == "AC") {
             siteCodex();
         } else if (clicado == "ACN") {
             siteNetwork();
         };
-    })
+    });
+
+
 
 
 
     function siteCodex() {
-        feedContent.append('<div id="loading" align="CENTER"><img src="img/wait.gif"></div>');
+        feedContent.html('<div id="loading" align="CENTER"><img src="img/wait.gif"></div>');
         getFeed(codex);
     }
 
@@ -29,66 +34,27 @@ $(document).on("pageinit", "#main", function() {
             url: url,
             dataType: 'jsonp',
             success: function(data, textStatus, jqXHR) {
-                console.log(data);
-                populateFeed(data);
+                console.log(textStatus);
+                item = data;
+                populateFeed(item);
             },
-
         });
-
 
     }
 
     function populateFeed(response) {
         feedContent.html("");
+        var num = 1;
         $.each(response.posts, function(i, newsItem) {
-            feedContent.append("<li>Titulo: " + newsItem.title + "<br>Data: " + newsItem.date + "<br>Autor: " + newsItem.author.name + "</li>");
+
+            if (num % 2 != 0) {
+                feedContent.append("<div class='ui-block-a'><a class='ui-btn ui-shadow ui-corner-all' href='" + newsItem.url + "'>" + newsItem.title + "</a>  </div>  ");
+            } else {
+                feedContent.append("<div class='ui-block-b'><a class='ui-btn ui-shadow ui-corner-all' href='" + newsItem.url + "'>" + newsItem.title + "</a> </div>   ");
+            }
+
+            num += 1;
         });
     }
 
-
-
-
-
-
-
-    // function getClicado(clicked) {
-    //     $("#segundoContent").append('<img src="img/wait.gif">');
-    //     if (clicked == "AC") {
-    //         $.mobile.navigate('#feed');
-    //         $(".headBar h1").html(" Feed");
-
-    //         var url = "http://animecodex.com.br/api/get_recent_posts/"
-    //         $.ajax({
-    //             url: url,
-    //             type: 'GET',
-    //             dataType: 'jsonp',
-    //         })
-    //             .success(function(a) {
-    //                 console.log(a);
-    //                 alert("success");
-    //                 $("#segundoContent").html("Sucesso");
-    //             })
-    //             .fail(function() {
-    //                 console.log("error");
-    //                 alert("error!");
-    //                 $("#segundoContent").html("deu erro");
-    //             })
-    //             .complete(function() {
-    //                 console.log("complete");
-    //             });
-
-
-    //         //$("#segundoContent").html("AnimeCodex.Com");
-    //     } else if (clicked == "ACN") {
-    //         $.mobile.navigate('#feed');
-    //         $(".headBar h1").html(" Feed")
-    //         //$("#segundoContent").html("AnimeCodexNetwork.Com");
-    //     }
-
-    // }
-
-
-    // function xmlParser(xml) {
-    //     $('#segundoContent').html(JSON.stringify(response));
-    // }
 })
